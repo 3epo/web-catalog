661e8386f3d21fc244b2be0d11996a2980d69b2b
Андрей <andrey3epo@gmail.com>
2022-06-26 21:53:21 +0500

    Second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const notionDocId = "51d76894c0df4aff8892185e366ea270"
+const notionDocId = "5c71cee970d8456ea6c69977cc126d82"
 
 fetch("https://potion-api.now.sh/html?id=" + notionDocId)
   .then(res => res.text())
