6fff3b0dcd65b58940002f492b69599346b5a468
Андрей <andrey3epo@gmail.com>
2022-06-28 12:39:37 +0500

    update 28.06

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const notionDocId = "5c71cee970d8456ea6c69977cc126d82"
+const notionLeft = "5c71cee970d8456ea6c69977cc126d82"
+const notionCenter = "509976f2aeb34b438ac4f28d4e31b3ce"
+const notionRight = "79a0e520008547b1a2a9206346d01be1"
 
-fetch("https://potion-api.now.sh/html?id=" + notionDocId)
+fetch("https://potion-api.now.sh/html?id=" + notionLeft)
   .then(res => res.text())
   .then(text => {
-    document.querySelector("main").innerHTML = text
-  })
+    document.querySelector("left").innerHTML = text
+})
+
+fetch("https://potion-api.now.sh/html?id=" + notionRight)
+  .then(res => res.text())
+  .then(text => {
+    document.querySelector("right").innerHTML = text
+})
+
+fetch("https://potion-api.now.sh/html?id=" + notionCenter)
+  .then(res => res.text())
+  .then(text => {
+    document.querySelector("cent").innerHTML = text
+})
